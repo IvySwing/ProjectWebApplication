ffa29395f29a629329086d587ef0e35bc0076010
Kori Riddick <entense@att.net>
2019-01-17 16:23:59 -0500

    updates to second story

---
+++ process2.php
@@ -0,0 +1,43 @@
+<?php
+$vs = $_POST['vs'];
+$bp = $_POST['bp'];
+$ab = $_POST['ab'];
+$sv = $_POST['sv'];
+$tb = $_POST['tb'];
+$fb = $_POST['fb'];
+$tv = $_POST['tv'];
+$ad = $_POST['ad'];
+$sa = $_POST['sa'];
+$ta = $_POST['ta'];
+$hb = $_POST['hb'];
+$xb = $_POST['xb'];
+$yb = $_POST['yb'];
+$jb = $_POST['jb'];
+$fs = $_POST['fs'];
+$no = $_POST['no'];
+$vb = $_POST['vb'];
+$nn = $_POST['nn'];
+$pb = $_POST['pb'];
+$eb = $_POST['eb'];
+$nb = $_POST['nb'];
+$av = $_POST['av'];
+$sd = $_POST['sd'];
+$en = $_POST['en'];
+$gv = $_POST['gv'];
+$la = $_POST['la'];
+$lv = $_POST['lv'];
+
+
+$a = array($vs, $bp, $ab, $sv, $tb, $fb, $tv, $ad, $sa, $ta, $hb, $xb, $yb, $jb, $fs, $no, $vb, $nn, $pb, $eb, $nb, $av, $sd, $en, $gv, $la, $lv
+);
+
+$d = file_get_contents('data2.json');
+$d = json_decode($d, true);
+
+array_unshift($d, $a);
+
+$d = json_encode($d);
+file_put_contents('data2.json', $d);
+
+header('location:../story2.php');
+?>
---
+++ story2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <body>
+        <?php
+        
+        $d = file_get_contents('data2.json');
+        $d = json_decode($d, true);
+        
+            $a = $d[0];
+    
+            echo "He " .$a[0]. " his " .$a[1]. " so his " .$a[2]. " pushes against me ... Yes. Right there ... He " .$a[3]. " his " .$a[4]. " along my " .$a[5]. ", eases back, then " .$a[6]. " into me again - so " .$a[7]. ", so " .$a[8]. ", so " .$a[9]. " - his " .$a[10]. " pressing down on me, his " .$a[11]. " and his " .$a[12]. " on either side of my " .$a[13]. ". Oh, Ana! He " .$a[14]. " as he lets go, my name a " .$a[15]. " on his " .$a[16]. " as he finds his " .$a[17]. ". His " .$a[18]. " rests on my " .$a[19]. ", his " .$a[20]. " wrapped around me ... I just want to enjoy the " .$a[21]. " " .$a[22]. " afterglow of making " .$a[23]. " with Christian Grey, because that's what we've done: " .$a[24]. ", " .$a[25]. " " .$a[26]. ".";
+        ?>
+    </body>
+</html>
