e5a7ee6cee47256e2dc280fa57e4a95c3f457651
Kori Riddick <entense@att.net>
2019-01-23 22:23:04 -0500

    final tweaks on styling

--- story2.php
+++ story2.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <h1> The Novel: Fifty Shades of #5C564A </h1>
+        <p>
         <?php
         
         $d = file_get_contents('data2.json');
@@ -15,5 +16,9 @@
     
             echo "He " .$a[0]. " his " .$a[1]. " so his " .$a[2]. " pushes against me ... Yes. Right there ... He " .$a[3]. " his " .$a[4]. " along my " .$a[5]. ", eases back, then " .$a[6]. " into me again - so " .$a[7]. ", so " .$a[8]. ", so " .$a[9]. " - his " .$a[10]. " pressing down on me, his " .$a[11]. " and his " .$a[12]. " on either side of my " .$a[13]. ". Oh, Ana! He " .$a[14]. " as he lets go, my name a " .$a[15]. " on his " .$a[16]. " as he finds his " .$a[17]. ". His " .$a[18]. " rests on my " .$a[19]. ", his " .$a[20]. " wrapped around me ... I just want to enjoy the " .$a[21]. " " .$a[22]. " afterglow of making " .$a[23]. " with Christian Grey, because that's what we've done: " .$a[24]. ", " .$a[25]. " " .$a[26]. ".";
         ?>
+        </p>
+        <br>
+        <footer><a href="index.html">Wanna Try Another One?</a>
+        </footer>
     </body>
 </html>
--- story3.php
+++ story3.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <h1> The Stump Speech </h1>
+        <p>
         <?php
         
         $d = file_get_contents('data3.json');
@@ -15,5 +16,9 @@
     
             echo "Ladies and Gentlemen, on this " .$a[0]. " occasion it is a privilege to address such a/an " .$a[1]. "-looking group of " .$a[2]. ". I can tell from your smiling " .$a[3]. " that you will support the " .$a[4]. " program in the coming election. I promise that, if elected, there will be a/an " .$a[5]. " in every " .$a[6]. " and two " .$a[7]. " in every garage. I want to warn you against my " .$a[8]. " opponent, Mr. " .$a[9]. ". This man is nothing but a/an " .$a[10]. " " .$a[11]. ". He is of " .$a[12]. " character and is working with " .$a[13]. " in glove with the criminal element. If elected, I promise to eliminate vice. I will keep the " .$a[13]. " off the city's streets. I will keep crooks from dipping their " .$a[14]. " in the public till. I promise you " .$a[15]. " government, " .$a[16]. " taxes, and " .$a[17]. " schools."
         ?>
+        </p>
+        <br>
+        <footer><a href="index.html">Wanna Try Another One?</a>
+        </footer>
     </body>
 </html>
